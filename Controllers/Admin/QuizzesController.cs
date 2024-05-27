eddb30766b5df1b9fc4eb00342f7aa4f0c11bed5
Te1ne <nq2019thaigiabao230504@gmail.com>
2024-05-27 19:37:28 +0700

    CRUD of item dtb

---
+++ Controllers/Admin/QuizzesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace study4_be.Controllers.Admin
+{
+    public class QuizzesController : Controller
+    {
+        public IActionResult Quizzes_List()
+        {
+            return View();
+        }
+        public IActionResult Quizzes_Create()
+        {
+            return View();
+        }
+        public IActionResult Quizzes_Delete()
+        {
+            return View();
+        }
+        public IActionResult Quizzes_Edit()
+        {
+            return View();
+        }
+        public IActionResult Quizzes_Details()
+        {
+            return View();
+        }
+    }
+}
