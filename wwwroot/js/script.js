e6e67f8a3a7298cb42b0359fd20080575b1b58d2
Te1ne <nq2019thaigiabao230504@gmail.com>
2024-05-29 15:55:50 +0700

    Revert

--- wwwroot/js/script.js
+++
@@ -1,9 +0,0 @@
-﻿function ChangeImage(UploadImage, previewImg) {
-    if (UploadImage.files && UploadImage.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            document.getElementById('previewImg').setAttribute('src', e.target.result);
-        }
-        reader.readAsDataURL(UploadImage.files[0])
-    }
-}
