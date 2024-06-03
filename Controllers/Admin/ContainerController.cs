f76c1869f764d522c5c569bdf5ce89f4026a9e16
Te1ne <nq2019thaigiabao230504@gmail.com>
2024-06-03 20:06:56 +0700

    container

---
+++ Controllers/Admin/ContainerController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using study4_be.Models;
+using study4_be.Repositories;
+
+namespace study4_be.Controllers.Admin
+{
+	public class ContainerController : Controller
+	{
+		private readonly ILogger<ContainerController> _logger;
+		public ContainerController(ILogger<ContainerController> logger)
+		{
+			_logger = logger;
+		}
+		private readonly ContainerRepository _containersRepository = new ContainerRepository();
+		public STUDY4Context _context = new STUDY4Context();
+
+		[HttpGet("GetAllCourses")]
+		//public async Task<ActionResult<IEnumerable<Course>>> GetAllCourses()
+		//{
+		//	var courses = await _containersRepository.GetAllCoursesAsync();
+		//	return Json(new { status = 200, message = "Get Courses Successful", courses });
+
+		//}
+		//development enviroment
+		[HttpDelete("DeleteAllCourses")]
+		public async Task<IActionResult> DeleteAllCourses()
+		{
+			await _containersRepository.DeleteAllUnitsAsync();
+			return Json(new { status = 200, message = "Delete Containers Successful" });
+		}
+		public async Task<IActionResult> Container_List()
+		{
+			//var courses = await _containersRepository.GetAllCoursesAsync();
+			//return View(courses);
+			return View();
+		}
+		public IActionResult Container_Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Container_Create(Container container)
+		{
+			if (!ModelState.IsValid)
+			{
+
+				return View(container);    //show form with value input and show errors
+			}
+			try
+			{
+				try
+				{
+					await _context.AddAsync(container);
+					await _context.SaveChangesAsync();
+					CreatedAtAction(nameof(GetContainerById), new { id = container.ContainerId }, container);
+				}
+				catch (Exception e)
+				{
+					CreatedAtAction(nameof(GetContainerById), new { id = container.ContainerId }, container);
+					_logger.LogError(e, "Error occurred while creating new course.");
+				}
+				return RedirectToAction("Index", "Home"); // nav to main home when add successfull, after change nav to index create Courses
+			}
+			catch (Exception ex)
+			{
+				// show log
+				_logger.LogError(ex, "Error occurred while creating new course.");
+				ModelState.AddModelError("", "An error occurred while processing your request. Please try again later.");
+				return View(container);
+			}
+		}
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetContainerById(int id)
+		{
+			var container = await _context.Containers.FindAsync(id);
+			if (container == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(container);
+		}
+
+		public IActionResult Container_Edit()
+		{
+			return View();
+		}
+
+		public IActionResult Container_Delete()
+		{
+			return View();
+		}
+
+		public IActionResult Container_Details()
+		{
+			return View();
+		}
+	}
+}
