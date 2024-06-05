d573ada35352d9edfb4e94e26ae42bc1bcdfb4c3
Phan Văn Năng <phanvannang004@gmail.com>
2024-06-05 21:03:31 +0700

    api[Get] get All Outstanding user hadn't bought before

--- Controllers/API/Courses_APIController.cs
+++ Controllers/API/Courses_APIController.cs
@@ -8,6 +8,8 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using study4_be.Services;
 using Microsoft.EntityFrameworkCore;
+using study4_be.Services.Request;
+using System.Linq;
 namespace study4_be.Controllers.API
 {
     [Route("api/[controller]")]
@@ -22,13 +24,61 @@ namespace study4_be.Controllers.API
         private readonly CourseRepository _coursesRepository = new CourseRepository();
         public STUDY4Context _context = new STUDY4Context();
 
-        [HttpGet("Get_AllCourses")]
+        [HttpGet("Get_AllCourses")] 
         public async Task<ActionResult<IEnumerable<Course>>> Get_AllCourses()
         {
             var courses = await _coursesRepository.GetAllCoursesAsync();
             return Json(new { status = 200, message = "Get Courses Successful", courses });
         }
-        //development enviroment
+
+        [HttpPost("Get_UnregisteredCourses")] // courses the user hasn't registered for
+        public async Task<ActionResult> Get_UnregisteredCourses(GetUserCoursesRequest request)
+        {
+            List<int> registeredCourseIds = await GetCoursesUserHasPurchasedAsync(request.userId);
+
+            var unregisteredCourses = await _context.Courses
+                .Where(c => !registeredCourseIds.Contains(c.CourseId)) // Sử dụng CourseId từ bảng Courses
+                .ToListAsync();
+
+            if (unregisteredCourses == null || !unregisteredCourses.Any())
+            {
+                return NotFound(new { status = 404, message = "No unregistered courses found or User Register All Before" });
+            }
+
+            return Ok(new { status = 200, message = "Get Unregistered Courses Successful", unregisteredCourses });
+        }
+        [HttpPost("Get_OutstandingCoursesUserNotBought")]
+        public async Task<ActionResult> Get_OutstandingCoursesUserNotBought(GetUserCoursesRequest req)
+        {
+            List<int>  userPurchasedCourses = await GetCoursesUserHasPurchasedAsync(req.userId);
+            var outstandingCourses = await _context.UserCourses
+                .Where(uc => !userPurchasedCourses.Contains(uc.CourseId)) // Lọc các khóa học chưa mua
+                .GroupBy(uc => uc.CourseId) // Nhóm theo CourseId
+                .OrderByDescending(g => g.Count()) // Sắp xếp giảm dần theo số lần xuất hiện
+                .Select(g => g.Key)// Chọn ra CourseId
+                .Take(req.amountOutstanding)
+                .ToListAsync();
+
+            // Lấy thông tin chi tiết của các khóa học nổi bật
+            var detailedOutstandingCourses = await _context.Courses
+                .Where(c => outstandingCourses.Contains(c.CourseId)) // Lọc các khóa học theo danh sách các CourseId nổi bật
+                .ToListAsync();
+                
+            if (detailedOutstandingCourses == null || !detailedOutstandingCourses.Any())
+                {
+                    return NotFound(new { status = 404, message = "No outstanding courses found or not have any outstaind course" });
+                }
+            return Ok(new { status = 200, message = "Get Outstanding Courses User Hadn't Bought Successful", outstandingCourses = detailedOutstandingCourses });
+        }
+        public async Task<List<int>> GetCoursesUserHasPurchasedAsync(string userId)
+        {
+                var userPurchaseBought = await _context.UserCourses
+                    .Where(uc => uc.UserId == userId)
+                    .Select(uc => uc.CourseId)
+                    .ToListAsync();
+                return userPurchaseBought;
+        }
+
         [HttpDelete("Delete_AllCourses")]
         public async Task<IActionResult> Delete_AllCourses()
         {
---
+++ Services/Request/GetUserCoursesRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace study4_be.Services.Request
+{
+    public class GetUserCoursesRequest
+    {
+        public string userId { get; set; } = string.Empty;
+        public int amountOutstanding {  get; set; } = 0; // Default value set to 0
+    }
+}
