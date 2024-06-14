c551726803cda60823f1832dcffede3c928f7992
Phan Văn Năng <109004626+YunneeToiChoi@users.noreply.github.com>
2024-06-14 16:37:43 +0700

    new api video controller

---
+++ Controllers/API/Video_APIController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using study4_be.Models;
+using study4_be.Repositories;
+using study4_be.Services.Request;
+
+namespace study4_be.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Video_APIController : Controller
+    {
+        private readonly ILogger<Video_APIController> _logger;
+        public STUDY4Context _context = new STUDY4Context();
+        public Video_APIController(ILogger<Video_APIController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost("Get_AllVideoOfLesson")]
+        public async Task<IActionResult> Get_AllVideoOfLesson(OfLessonRequest _req)
+        {
+            var video = _context.Videos.Where(v => v.LessonId == _req.lessonId);
+            var lessonTag = await _context.Lessons
+                .Where(l => l.LessonId == _req.lessonId)
+                .Select(l => l.Tag)
+                .FirstAsync();
+            var lessonTagResponse = new
+            {
+                lessonTag = lessonTag.TagId
+            };
+            return Json(new { status = 200, message = "Get All Video of lesson Successful", data = video, lessonTag = lessonTagResponse});
+        }
+    }
+}
---
+++ Services/Request/OfLessonRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace study4_be.Services.Request
+{
+    public class OfLessonRequest
+    {
+        public int lessonId { get; set; }
+    }
+}
